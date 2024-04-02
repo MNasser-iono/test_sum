38fe7a3421202d4b46ef0230b8096a6cb3fc2bf9
MNasser-iono <mahmoud.nasser@iono-tech.com>
2024-04-02 14:04:05 +0200

    sec commet

--- test/test_sum_function.c
+++ test/test_sum_function.c
@@ -17,12 +17,12 @@ void test_sum_function_NeedToImplement(void)
     TEST_IGNORE_MESSAGE("Need to Implement sum_function");
 }
 
-void test_sum_positive_numbers(void)
-{
-    TEST_ASSERT_EQUAL_INT(5, sum(2, 3));
-    TEST_ASSERT_EQUAL_INT(10, sum(5, 5));
-    TEST_ASSERT_EQUAL_INT(15, sum(10, 5));
-}
+// void test_sum_positive_numbers(void)
+// {
+//     TEST_ASSERT_EQUAL_INT(5, sum(2, 3));
+//     TEST_ASSERT_EQUAL_INT(10, sum(5, 5));
+//     TEST_ASSERT_EQUAL_INT(15, sum(10, 5));
+// }
 
 void test_sum_negative_numbers(void)
 {
